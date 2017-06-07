04d6e1ef1d1bf0a9e06ac390add061a8189fcf19
Ananddm <anand.muruganantham01@gmail.com>
2017-06-07 14:24:32 +0530

    Create Stringint.java

---
+++ Stringint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class Stringint
+{
+public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+String p=new String();
+p=sc.next();
+int n=Integer.parseInt(p);
+System.out.println(n);
+}
+}
