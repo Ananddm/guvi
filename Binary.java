35abc94d29936c24b1a46eb43dd6029f9d7c0325
Ananddm <anand.muruganantham01@gmail.com>
2017-06-08 11:58:43 +0530

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+ class Binary
+ { 
+	public static void main(String args[])
+ 	{ 
+         Scanner sr=new Scanner(System.in);
+         int n=sr.nextInt();
+         System.out.println(Integer.toBinaryString(n));
+}}
