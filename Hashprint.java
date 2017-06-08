447ed1e88ef68f31305b7148ed813f3ef831f302
Ananddm <anand.muruganantham01@gmail.com>
2017-06-08 12:09:10 +0530

    Create Hashprint.java

---
+++ Hashprint.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+ class Hashprint
+ { 
+	public static void main(String args[])
+ 	{ int i;
+ 	for(i=0;i<4;i++)
+         {
+             for(int j=0;j<=i;j++)
+             {
+            System.out.print("#");}
+           System.out.println();
+         }
+}}
